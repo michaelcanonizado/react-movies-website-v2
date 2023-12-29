f9f4e57a5b603aacded8146655cbe4498bb76d75
Michael Canonizado <michaelxaviercanonizado@gmail.com>
2023-12-29 15:42:34 +0800

    copied tailwind configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,41 @@
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
-    },
-  },
-  plugins: [],
-}
-export default config
+	content: [
+		'./src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/components/**/*.{js,ts,jsx,tsx,mdx}',
+		'./src/app/**/*.{js,ts,jsx,tsx,mdx}',
+	],
+	theme: {
+		fontFamily: { serif: ['Roboto'], display: ['Bebas Neue'] },
+		screens: {
+			xs: '250px',
+			sm: '350px',
+			md: '600px',
+			lg: '1024px',
+			xl: '1280px',
+		},
+		extend: {
+			colors: {
+				primary: '#000000',
+				secondary: {
+					100: '#2C2C2C',
+					200: '#1A1A1A',
+					300: '#121212',
+					400: '#FFFFFFB3',
+				},
+				accent: {
+					100: '#E50914',
+					200: '#5799EF',
+					300: '#18B7D9',
+				},
+				neutral: '#FFFFFF',
+			},
+			aspectRatio: {
+				poster: '2 / 3',
+			},
+		},
+	},
+	plugins: [],
+};
+export default config;
